feb5c884d991920d53594a18d5f6855b9379aef1
naim380 <75119537+naim380@users.noreply.github.com>
2021-03-11 10:34:26 +0100

    exo 5 fini

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 //5-1 Import
 
 import {hello,showError} from './functions.js';
+//import {default as niceFonction} from './functions.js';
 
 hello();
 showError('naim');
 
+//niceFonction();
 
 // 5-2 Export
 const [a, b] = [7, 9];
+import {sum, sub, multiply, divide, pow} from './math.js';
+console.log(sum(a,b))
+console.log(sub(a,b))
+console.log(multiply(a,b))
+console.log(divide(a,b))
+console.log(pow(a,b))
